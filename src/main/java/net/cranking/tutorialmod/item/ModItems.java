78a2e33ac49d9608274c7887ec26e95b6f2a424e
Trolltuesday <trebornewbor@gmail.com>
2025-03-18 20:59:54 -0500

    s

--- src/main/java/net/cranking/tutorialmod/item/ModItems.java
+++ src/main/java/net/cranking/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.cranking.tutorialmod.item;
 
 import net.cranking.tutorialmod.TutorialMod;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,8 +12,10 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            ()-> new Item(new Item.Properties()));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
-        public static final RegistryObject<Item>
     }
 }
